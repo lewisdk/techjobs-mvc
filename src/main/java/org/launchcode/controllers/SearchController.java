c8cc46ef0e5a49585a62410d3f6aed70bc4556f9
Deirdre Lewis <lewis.deirdre.k@gmail.com>
2019-04-24 13:35:07 -0500

    list jobs task added

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
